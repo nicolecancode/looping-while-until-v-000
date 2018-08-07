3a19f473c02a4d4f6586573b5682b943b65e1639
Nicole Pretorius <nicole.pretoriusuk+learn@gmail.com>
2018-08-07 12:42:10 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
-        counter == 10
+        using_until == 10
         levitation_force = 6
         puts "Wingardium Leviosa"
-        counter += 1
+        using_until += 1
 
 end
